084f90d3e78d78c77805eb6e99e74c7fa5c85e0b
Md. Asaduzzaman Noor <md.noor.asad@gmail.com>
2022-01-01 12:58:17 +0600

    Problem name > FrogJmp

---
+++ FrogJmp.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int X, int Y, int D) {
+        if(X == Y) return 0;
+        if((Y - X) % D == 0) 
+            return ((Y - X)/D);
+        else
+            return ((Y - X)/D) + 1;
+    }
+}
